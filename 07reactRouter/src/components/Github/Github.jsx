c4a3ea8dd6a83f11215638ee0a8ddb97cdcc36c4
Vishal Kamble <vishalk@supportcandy.net>
2024-06-05 16:00:59 +0530

    react router crash course

---
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
+
+function Github() {
+
+    const data = useLoaderData()
+
+    // const [data, setData] = useState([])
+    // useEffect( () => {
+    //     fetch('https://api.github.com/users/vishalkamble95')
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         console.log(data);
+    //         setData(data)
+    //     })
+    // })
+  return (
+    <div className='m-4 bg-gray-600 text-white text-3xl p-4 flex items-center justify-center'>Github Followers: {data.followers}
+    <img src={data.avatar_url} alt="Github picture" width={300} />
+    </div>
+  )
+}
+
+export default Github
+
+export const githubInfoLoader = async () => {
+    const response = await fetch('https://api.github.com/users/vishalkamble95')
+    return response.json()
+}
